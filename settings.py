67419e815ef6bc4be5aab29936bc14134a3482f2
plazmotron <steinar.a.volden@gmail.com>
2022-01-28 00:29:55 +0300

    Some comments

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
-LETTERS = 5
-TRIES = 6
-RUNS = 5
-AUTOMATIC = True
-REVERSE = True
+LETTERS = 5         # How many letters do you want in the word
+TRIES = 6           # How many tries do you want
+RUNS = 5            # Number of game rounds
+AUTOMATIC = True    # True: Machine guesses, False: You are taking the challenge yourself
+REVERSE = True      # If you want to make a word for the machine to guess yourself
+# Also you can set AUTOMATIC and REVERSE to False and guess your own words. Why would you do it? Try asking yourself.
